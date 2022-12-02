a8e8430a8b5d4d6a51ceee4e9b71e5fcf1c4ae26
apazala <albertopascual@hotmail.es>
2022-12-02 14:59:36 +0100

    update like problem b

--- Program.cs
+++ Program.cs
@@ -1,19 +1,30 @@
 ﻿internal class Program
 {
+    static int[,] resultsTree = new int[3, 3];
+    private static void Init()
+    {
+        //rock choice
+        resultsTree[0, 0] = 1 + 3;
+        resultsTree[1, 0] = 1;
+        resultsTree[2, 0] = 1 + 6;
+        //paper choice
+        resultsTree[0, 1] = 2 + 6;
+        resultsTree[1, 1] = 2 + 3;
+        resultsTree[2, 1] = 2;
+        //scissors choice
+        resultsTree[0, 2] = 3;
+        resultsTree[1, 2] = 3 + 6;
+        resultsTree[2, 2] = 3 + 3;
+    }
     private static void Main(string[] args)
     {
-        const int diff = 'X'-'A';
-        int[] results = new int [26];
-        results[diff] = 3; //A X, B Y, C Z
-        results[diff+1] = 6; //A Y, B Z
-        results[diff-2] = 6; //X C
+        Init();
+        
         int total = 0;
         string[] lines = File.ReadAllLines("input.txt");
-        foreach(string line in lines){
-            char cP1 = line[0];
-            char cP2 = line[2];
-            total += 1 + cP2 - 'X';
-            total += results[cP2-cP1]; 
+        foreach (string line in lines)
+        {
+            total += resultsTree[line[0]-'A', line[2]-'X'];
         }
 
         Console.WriteLine(total);
